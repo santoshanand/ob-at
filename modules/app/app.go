502234dd2940c950b0d336b43a551b6bbb1799db
santoshanand <santoshanand.jogi@gmail.com>
2023-03-11 10:30:21 +0530

    added tailwind

--- modules/app/app.go
+++ modules/app/app.go
@@ -1,6 +1,9 @@
 package app
 
 import (
+	"html/template"
+	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
@@ -20,14 +23,29 @@ const idleTimeout = 5 * time.Second
 // RunApp - it will run fiber application
 func RunApp(log *zap.SugaredLogger, cfg *config.Config) *fiber.App {
 	engine := html.New("./views", ".html")
+	engine.AddFunc("getCssAsset", func(name string) (res template.HTML) {
+		filepath.Walk("public/styles", func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			if info.Name() == name {
+				res = template.HTML("<link rel=\"stylesheet\" href=\"/" + path + "\">")
+			}
+			return nil
+		})
+		return
+	})
 	// engine := html.NewFileSystem(http.FS(viewsfs), ".html")
 	app := fiber.New(fiber.Config{
 		IdleTimeout: idleTimeout,
 		Views:       engine,
+		ViewsLayout: "layouts/main",
 	})
+
 	app.Use(recover.New())
 	app.Use(logger.New())
 
+	app.Static("/public", "./public")
 	app.Get("/", func(c *fiber.Ctx) error {
 		// Render index template
 		return c.Render("index", fiber.Map{
